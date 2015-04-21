07d74db763a8619ff72a1d755768eaaa55d4bff7
YaNn <yann.berton@adico.fr>
2015-04-21 11:51:13 +0200

    Fixes
    Change Theme name and state

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Adico theme Decouverte',
-	'description' => 'Theme Decouverte ',
+	'title' => 'Adico theme Orange',
+	'description' => 'Theme Orange ',
 	'category' => 'misc',
 	'version' => '0.0.1',
-	'state' => 'beta',
+	'state' => 'stable',
 	'uploadfolder' => true,
 	'createDirs' => '',
 	'clearcacheonload' => true,
